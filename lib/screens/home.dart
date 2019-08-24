190643397f69e2a134b9a90d8869b41c9c10acff
masterUNG <phrombutr@gmail.com>
2019-08-24 16:36:38 +0700

    finish 24Aug

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,8 +9,54 @@ class _HomeState extends State<Home> {
   // Explicit
 
   // Method
+  Widget mySizebox() {
+    return SizedBox(
+      width: 5.0,
+      height: 16.0,
+    );
+  }
+
+  Widget signInButton() {
+    return Expanded(
+      child: RaisedButton(
+        color: Colors.blue.shade400,
+        child: Text(
+          'Sign In',
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget signUpButton() {
+    return Expanded(
+      child: OutlineButton(
+        child: Text(
+          'Sign Up',
+          style: TextStyle(color: Colors.blue.shade400),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget showButton() {
+    return Container(
+      margin: EdgeInsets.only(left: 50.0, right: 50.0),
+      child: Row(
+        children: <Widget>[
+          signInButton(),
+          mySizebox(),
+          signUpButton(),
+        ],
+      ),
+    );
+  }
+
   Widget showLogo() {
-    return Container(alignment: Alignment.center,
+    return Container(
+      alignment: Alignment.center,
       child: Container(
         width: 150.0,
         height: 150.0,
@@ -20,18 +66,36 @@ class _HomeState extends State<Home> {
   }
 
   Widget showAppName() {
-    return Text('Ung UBRU');
+    return Text(
+      'Ung UBRU',
+      style: TextStyle(
+        fontSize: 35.0,
+        fontWeight: FontWeight.bold,
+        fontStyle: FontStyle.italic,
+        color: Colors.blue[400],
+        fontFamily: 'DancingScript',
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          showLogo(),
-          showAppName(),
-        ],
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: RadialGradient(
+          colors: [Colors.white, Colors.blue],
+          radius: 1.0,
+        )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            showLogo(),
+            mySizebox(),
+            showAppName(),
+            showButton(),
+          ],
+        ),
       ),
     );
   }
