bb09ae7fe8bb5832e77b3c7ffcd014557df03d45
masterUNG <phrombutr@gmail.com>
2019-08-24 15:33:10 +0700

    Image Asset OK

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,6 +9,16 @@ class _HomeState extends State<Home> {
   // Explicit
 
   // Method
+  Widget showLogo() {
+    return Container(alignment: Alignment.center,
+      child: Container(
+        width: 150.0,
+        height: 150.0,
+        child: Image.asset('images/logo.png'),
+      ),
+    );
+  }
+
   Widget showAppName() {
     return Text('Ung UBRU');
   }
@@ -16,9 +26,10 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(mainAxisAlignment: MainAxisAlignment.center,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          showAppName(),
+          showLogo(),
           showAppName(),
         ],
       ),
