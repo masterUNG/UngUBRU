1c548dbe208fbb71bcfd96c7ca01ee4352b21f57
masterUNG <phrombutr@gmail.com>
2019-08-25 11:17:53 +0700

    update  Form Finish

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -6,12 +6,89 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  // Explicit
+  Color nameColor = Colors.green.shade400;
+  Color emailColor = Colors.blue.shade400;
+  Color passwordColor = Colors.pink.shade400;
+
+  // Method
+
+  Widget nameText() {
+    return TextFormField(
+      decoration: InputDecoration(
+        icon: Icon(
+          Icons.face,
+          size: 36.0,
+          color: nameColor,
+        ),
+        labelText: 'Name :',
+        labelStyle: TextStyle(color: nameColor),
+        helperText: 'Type Your Display Name',
+        helperStyle: TextStyle(color: nameColor),
+        hintText: 'English only',
+      ),
+    );
+  }
+
+  Widget emailText() {
+    return TextFormField(
+      decoration: InputDecoration(
+        icon: Icon(
+          Icons.email,
+          size: 36.0,
+          color: emailColor,
+        ),
+        labelText: 'Email :',
+        labelStyle: TextStyle(color: emailColor),
+        helperText: 'Type Your Email',
+        helperStyle: TextStyle(color: emailColor),
+        hintText: 'you@email.com',
+      ),
+    );
+  }
+
+  Widget passwordText() {
+    return TextFormField(
+      decoration: InputDecoration(
+        icon: Icon(
+          Icons.lock,
+          size: 36.0,
+          color: passwordColor,
+        ),
+        labelText: 'Password :',
+        labelStyle: TextStyle(color: passwordColor),
+        helperText: 'Type Your Password',
+        helperStyle: TextStyle(color: passwordColor),
+        hintText: 'More 6 Charactor',
+      ),
+    );
+  }
+
+  Widget registerButton() {
+    return IconButton(
+      icon: Icon(Icons.cloud_upload),
+      onPressed: () {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color.fromARGB(0xFF, 0xFF, 0xA0, 0x00),
         title: Text('Register'),
-      ),body: Text('body'),
+        actions: <Widget>[
+          registerButton(),
+        ],
+      ),
+      body: ListView(
+        padding: EdgeInsets.only(left: 50.0, right: 50.0, top: 70.0),
+        children: <Widget>[
+          nameText(),
+          emailText(),
+          passwordText(),
+        ],
+      ),
     );
   }
 }
