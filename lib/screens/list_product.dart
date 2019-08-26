6e9df513d4a98cf09ac8395946ddfbb4a2ff29e4
masterUNG <phrombutr@gmail.com>
2019-08-26 16:10:19 +0700

    Connected Fire Stroe ok

--- lib/screens/list_product.dart
+++ lib/screens/list_product.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ListProduct extends StatefulWidget {
   @override
@@ -6,6 +9,32 @@ class ListProduct extends StatefulWidget {
 }
 
 class _ListProductState extends State<ListProduct> {
+  // Explicit
+  // List<String> names = ['aa', 'bb', 'cc'];
+  List<DocumentSnapshot> snapshots;
+
+  // Method
+  @override
+  void initState() {
+    super.initState();
+    readFireStore();
+  }
+
+  Future<void> readFireStore() async {
+    Firestore firestore = Firestore.instance;
+    CollectionReference collectionReference = firestore.collection('Product');
+    StreamSubscription<QuerySnapshot> subscription =
+        await collectionReference.snapshots().listen((dataSnapshot) {
+
+          snapshots = dataSnapshot.documents;
+          for (var mySnapshot in snapshots) {
+            String name = mySnapshot.data['Name'];
+            print('name = $name');
+          }
+
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Text('This is ListProduct');
