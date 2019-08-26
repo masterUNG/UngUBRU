00607041fd6605ba06deb58ec7be4bf727d340c1
masterUNG <phrombutr@gmail.com>
2019-08-26 11:08:58 +0700

    Authen OK

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -17,6 +17,72 @@ class _AuthenState extends State<Authen> {
         color: myColor,
         size: 36.0,
       ),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    );
+  }
+
+  Widget emailText() {
+    return TextFormField(
+      decoration: InputDecoration(
+        icon: Icon(
+          Icons.email,
+          size: 36.0,
+          color: myColor,
+        ),
+        labelText: 'Email :',
+        labelStyle: TextStyle(color: myColor),
+      ),
+    );
+  }
+
+  Widget passwordText() {
+    return TextFormField(
+      decoration: InputDecoration(
+        icon: Icon(
+          Icons.lock,
+          size: 36.0,
+          color: myColor,
+        ),
+        labelText: 'Password :',
+        labelStyle: TextStyle(color: myColor),
+      ),
+    );
+  }
+
+  Widget showName() {
+    return ListTile(
+      leading: ImageIcon(
+        AssetImage('images/logo.png'),
+        size: 36.0,
+        color: myColor,
+      ),
+      title: Text(
+        'Ung UBRU',
+        style: TextStyle(
+          fontSize: 30.0,
+          color: myColor,
+          fontFamily: 'DancingScript',
+        ),
+      ),
+    );
+  }
+
+  Widget showAuthen() {
+    return Container(
+      alignment: Alignment.center,
+      child: Container(
+        width: 250.0,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            showName(),
+            emailText(),
+            passwordText(),
+          ],
+        ),
+      ),
     );
   }
 
@@ -25,7 +91,10 @@ class _AuthenState extends State<Authen> {
     return Scaffold(
       body: SafeArea(
         child: Stack(
-          children: <Widget>[backButton(),],
+          children: <Widget>[
+            backButton(),
+            showAuthen(),
+          ],
         ),
       ),
     );
