c95c5d117c7e497966f5a8ff5491878b151b9a37
masterUNG <phrombutr@gmail.com>
2019-08-27 16:04:16 +0700

    Finish 27Aug

--- lib/screens/show_map.dart
+++ lib/screens/show_map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class ShowMap extends StatefulWidget {
   @override
@@ -6,8 +7,40 @@ class ShowMap extends StatefulWidget {
 }
 
 class _ShowMapState extends State<ShowMap> {
+  // Explicit
+  static const ubruLatLng = const LatLng(15.246847, 104.847532);
+  CameraPosition cameraPosition = CameraPosition(
+    target: ubruLatLng,
+    zoom: 16.0,
+  );
+
+  // Method
+
+  Set<Marker> myMarker() {
+    return <Marker>[
+      Marker(
+        position: ubruLatLng,
+        markerId: MarkerId('UBRU'),
+        infoWindow: InfoWindow(
+          title: 'ม.ราชภัฏอุบล',
+          snippet: 'มหาวิทยาลัยที่ยอดเยี่ยมที่สุด',
+        ),icon: BitmapDescriptor.defaultMarkerWithHue(60.0)
+      ),
+    ].toSet();
+  }
+
+  Widget myMap() {
+    return GoogleMap(
+      myLocationEnabled: true,
+      mapType: MapType.normal,
+      initialCameraPosition: cameraPosition,
+      onMapCreated: (GoogleMapController googleMapController) {},
+      markers: myMarker(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Show Map'),);
+    return myMap();
   }
 }
